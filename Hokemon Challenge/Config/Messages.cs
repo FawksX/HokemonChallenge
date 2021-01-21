b387ebfca5a5422441941c3bd48c9918b413ece4
FawksX <xfawks@gmail.com>
2021-01-21 15:57:14 +0000

    Update Messages.cs

--- Hokemon Challenge/Config/Messages.cs
+++ Hokemon Challenge/Config/Messages.cs
@@ -4,8 +4,8 @@ namespace HokemonChallenge.Config {
         public static string main_menu = "Type create to create a Pokemon\n" +
                                          "Type battle to battle two pokemon\n" +
                                          "Type exit to exit the program\n";
-        
-        public static string battle_start_error = "ERROR : You need atleast two pokemon in order to battle!"
+
+        public static string battle_start_error = "ERROR : You need atleast two pokemon in order to battle!";
 
     }
 }
