1d6bb3ef54ae0c38640b0e3fb0406448987b1554
FawksX <xfawks@gmail.com>
2021-01-25 14:23:11 +0000

    Remove Testing Data

--- Hokemon Challenge/Program.cs
+++ Hokemon Challenge/Program.cs
@@ -8,28 +8,6 @@ using HokemonChallenge.Util;
 namespace HokemonChallenge {
     class Program {
         public static void Main(string[] args) {
-            
-            Dictionary<PokeStats, int> pokeStatsDefault = new Dictionary<PokeStats, int>();
-            pokeStatsDefault.Add(PokeStats.Attack, 50);
-            pokeStatsDefault.Add(PokeStats.Defense, 50);
-            pokeStatsDefault.Add(PokeStats.Hp, 50);
-            pokeStatsDefault.Add(PokeStats.Speed, 50);
-            pokeStatsDefault.Add(PokeStats.SpecialAttack, 50);
-            pokeStatsDefault.Add(PokeStats.SpecialDefense, 50);
-
-            PokeCache.Add("poke1", Pokemon.Pokemon.Builder().Name("poke1")
-                .Rarity(EPokemonRarity.Normal)
-                .Team(EPokemonTeams.Instinct)
-                .Type(EPokemonTypes.Bug)
-                .CP(100)
-                .EV(pokeStatsDefault).IV(pokeStatsDefault).Build());
-            
-            PokeCache.Add("poke2", Pokemon.Pokemon.Builder().Name("poke2")
-                .Rarity(EPokemonRarity.Normal)
-                .Team(EPokemonTeams.Valor)
-                .Type(EPokemonTypes.Bug)
-                .CP(100)
-                .EV(pokeStatsDefault).IV(pokeStatsDefault).Build());
 
             while (true) {
                 
